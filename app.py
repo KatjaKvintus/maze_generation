4441e6fe3bc9a963df00eec98c0f8880547c884e
Katja Kvintus <katja.kvintus@helsinki.fi>
2023-04-18 19:00:55 +0300

    Added execution time counter

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template, request
 from . import backtracker
 from . import kruskal
+import time
 
 
 app = Flask(__name__)
@@ -21,20 +22,30 @@ def generate_mazes():
     size = request.form["maze_size"]
     size = int(size)
 
+    # Bactracker
+    start_time = time.time()
     backtracker_maze = backtracker.create_bactracker_maze(size)
+    end_time = time.time()
+    backtracker_execution_time = round((end_time - start_time) * 1000, 4)
     backtracker_image = backtracker.draw_maze_image(size, backtracker_maze)
     backtracker_impasses = backtracker.bactracker_maze_impasse_amount(backtracker_maze)
 
+    # Kruskal
+    start_time = time.time()
     kruskal_maze = kruskal.create_kruskal_maze(size)
+    end_time = time.time()
+    kruskal_execution_time = round((end_time - start_time) * 1000, 4)
     kruskal_image = kruskal.print_kruskal_maze(size, kruskal_maze)
     kruskal_impasses = kruskal.kruskal_maze_impasse_amount(kruskal_maze)
 
     return render_template("/results.html",
                            backtracker_maze=backtracker_maze,
                            backtracker_image=backtracker_image,
+                           backtracker_execution_time=backtracker_execution_time,
                            backtracker_impasses=backtracker_impasses,
                            kruskal_maze=kruskal_maze,
                            kruskal_image=kruskal_image,
+                           kruskal_execution_time = kruskal_execution_time,
                            kruskal_impasses=kruskal_impasses)
 
 
